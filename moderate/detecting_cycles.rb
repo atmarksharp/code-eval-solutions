f283dcb600581c73600d48517edeb2198f9202ba
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2014-08-01 22:05:21 +0900

    detecting_cycles added

---
+++ moderate/detecting_cycles.rb
@@ -0,0 +1,41 @@
+def detect(arr,len)
+	range = (-(len-1)..0).to_a.reverse + (1..(len-1)).to_a
+	for shift in range
+		res = detect_impl(arr,len,shift)
+		return res if res != nil
+	end
+
+	nil
+end
+
+def detect_impl(_arr,len,shift)
+	arr = nil
+	past = []
+
+	if shift >= 0
+		arr = _arr.reverse.drop(shift) + [nil]*shift
+	else
+		arr = [nil]*-shift + _arr.reverse.drop(-shift)
+	end
+
+	for i in (0..(arr.size/len-1))
+		cur = arr.drop(i*len).take(len)
+		if cur == past
+			return cur.reverse
+		end
+		past = cur
+	end
+
+	return nil
+end
+
+File.open(ARGV[0]).each_line do |line|
+	arr = line.split(" ")
+	for i in (0..arr.size-1)
+		res = detect(arr,i)
+		if res != nil
+			puts res.join(" ")
+			break
+		end
+	end
+end
