8a432ce2b7cab3e45897c73551bf331e3d1577f5
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2014-08-01 20:43:06 +0900

    longest_lines added

---
+++ moderate/longest_lines.rb
@@ -0,0 +1,22 @@
+def int_str?(s)
+  Integer(s)
+  true
+rescue ArgumentError
+  false
+end
+
+@num = nil
+@texts = []
+
+File.open(ARGV[0]).each_line do |line|
+    if int_str?(line)
+    	@num = line.to_i
+    else
+    	@texts.push(line)
+    end
+end
+
+@longs = @texts.sort_by{|s| s.size}.reverse.take(@num)
+@longs.each { |s|
+	puts s
+}
