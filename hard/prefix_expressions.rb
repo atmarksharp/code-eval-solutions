df1b7d8eabe849b5191e9d198440b0359f129ce1
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2014-08-02 20:59:35 +0900

    prefix_expressions added

---
+++ hard/prefix_expressions.rb
@@ -0,0 +1,53 @@
+OP = /^[+\-*\/]$/
+NUM = /^(?:[1-9][0-9]+|[0-9])$/
+
+def calc(op,arg1,arg2)
+	res = (
+		case op
+		when :+
+			arg1 + arg2
+		when :-
+			arg1 - arg2
+		when :*
+			arg1 * arg2
+		when :/
+			arg1 / arg2
+		end
+	)
+
+	return res
+end
+
+def parse(terms)
+	ops = []
+	nums = []
+
+	for s in terms
+		if (OP =~ s) == 0
+			ops.unshift s.to_sym
+		elsif (NUM =~ s) == 0
+			nums.push s.to_i
+		else
+			raise "Parse Error"
+		end
+	end
+
+	return [ops,nums]
+end
+
+def run(context)
+	ops,nums = context
+	for op in ops
+		arg1 = nums.shift.to_f
+		arg2 = nums.shift.to_f
+		nums.unshift calc(op,arg1,arg2)
+	end
+
+	return nums.first.round.to_i
+end
+
+File.open(ARGV[0]).each_line do |line|
+	terms = line.split(" ")
+	context = parse(terms)
+	puts run(context)
+end
